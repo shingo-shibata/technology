06984b7f22af29a4cb39dd4c6ec00f80d6c8e046
Shingo Shibata <munjapan@hotmail.com>
2018-01-17 07:44:27 +0000

    revised

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,17 @@ Rails.application.configure do
   
   # トラッキングIDを記載
   GA.tracker = "UA-110698421-1"
+  
+  # メール送信
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  	address:               'smtp.gmail.com',
+  	port:      	           587,
+  	domain:                'gmail.com',
+  	user_name:             Rails.application.secrets.mail,
+  	password:              Rails.application.secrets.pass,
+  	authentication:        'plain',
+  	enable_starttls_auto:  true
+  }
+
 end
