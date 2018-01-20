219680a37689b1ede15402514665d065f5f88c24
Shingo Shibata <munjapan@hotmail.com>
2018-01-20 07:35:35 +0000

    meta tag

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'google-analytics-rails', '1.1.1'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem "meta-tags"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+    
+    def default_meta_tags
+      {
+        site: Settings.site.name,
+        reverse: true,
+        title: Settings.site.page_title,
+        description: Settings.site.page_description,
+        keywords: Settings.site.page_keywords,
+        canonical: request.original_url,
+        og: {
+          title: :title,
+          type: Settings.site.meta.ogp.type,
+          url: request.original_url,
+          image: image_url(Settings.site.meta.ogp.image_path),
+          site_name: Settings.site.name,
+          description: :description,
+          locale: 'ja_JP'
+        }
+      }
+    end
+    
 end
